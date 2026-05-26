5813684e468de5b07d4651df4599b83c59a85a0f
lizethvannesajimenez-rgb <lizethvannesajimenez@gmail.com>
2026-05-26 00:12:37 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor activo');
+});
+
+app.get('/saludo', (req, res) => {
+  res.send('Hola comunidad');
+});
+
+app.get('/mensaje/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send('Hola ' + nombre);
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
+
+
+app.get('/nombre-ruta', (req, res) => {
+    res.send('Tu mensaje aquí');
+});
+
+app.post('/reporte', (req, res) => {
+  const mensaje = req.body.mensaje;
+  res.json({
+    estado: "Reporte recibido",
+    mensaje: mensaje
+  });
+});
